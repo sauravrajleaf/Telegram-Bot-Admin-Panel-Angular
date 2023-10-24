acdae7a6fce2952f269bf0b017c9ccccd2a44c6d
saurav raj <saurved785@gmail.com>
2023-10-25 03:03:16 +0530

    commented unnecessary code

--- src/app/environment.service.ts
+++ src/app/environment.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environment';
+// import { Injectable } from '@angular/core';
+// // import { environment } from 'src/environment';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class EnvironmentService {
-  constructor() {}
-  get googleClientId(): string {
-    return environment.googleClientId;
-  }
-}
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class EnvironmentService {
+//   constructor() {}
+//   // get googleClientId(): string {
+//   //   return environment.googleClientId;
+//   // }
+// }
